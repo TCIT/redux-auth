7b2acc996daaef1f5d071245d41a3a200e28ded9
Daniel <daniel@tcit.cl>
2019-04-04 16:45:22 -0300

    latest release

--- webpack.release.js
+++ webpack.release.js
@@ -67,7 +67,7 @@ module.exports = {
     }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    // new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
