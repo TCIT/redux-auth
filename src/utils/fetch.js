46a8483a5846115f4eda7e3c597d113302b6ae4c
Daniel <daniel@tcit.cl>
2019-11-14 10:09:52 -0300

    asfdasf

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -77,7 +77,7 @@ function updateAuthCredentials(resp) {
     // }
 
     // persist headers for next request
-    if (newHeaders.length) {
+    if (Object.values(newHeaders).length) {
       persistData(C.SAVED_CREDS_KEY, newHeaders);
     }
   }
